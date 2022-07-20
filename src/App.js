ab0d86469310329a1b448a9427231adda0281bd8
Eriche Gonzales <erichelouise@gmail.com>
2022-07-20 14:05:30 -0400

    Added dynamic background colors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 import Fighter from './components/Fighter'
 
 function App() {
-  const fighters = ['Lucina', 'Yoshi', 'Kirby', 'Mewtwo', 'Roy', 'Mario', 'Ness', 'Zelda', 'Kirby', 'Luigi', 'Ike', 'Sora']
+  const fighters = [
+    { name: 'Yoshi', color: '#89CC6A'}, 
+    { name: 'Lucina', color: '#81A7CC'}, 
+    { name: 'Kirby', color: '#FFD6E8'}, 
+    { name: 'Mewtwo', color: '#957BC8'}, 
+    { name: 'Pikachu', color: '#FFB916'}, 
+    { name: 'Mario', color: '	#FF412D'}, 
+    { name: 'Ness', color: '#E14041'}, 
+    { name: 'Zelda', color: '	#FFEAAA'}, 
+    { name: 'Pit', color: '#B1DAFF'}, 
+    { name: 'Ike', color: '#B94E41'}, 
+    { name: 'Luigi', color: '#62A446'}, 
+    { name: 'Jigglypuff', color: '#F79BFA'}, 
+  ]
   return (
     <div className="App">
       <h1>Fighters</h1>
